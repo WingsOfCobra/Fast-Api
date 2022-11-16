43ffbda3523f778932915790ffe21670e5510b81
WingsOfCobra <anian@sollingers.de>
2022-11-16 13:31:33 +0100

    updates

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-import tempfile
+import os, sys
+import shutil
+import time
 from fastapi import FastAPI, UploadFile, File
 from PIL import Image, ImageSequence
-import os, io
-import shutil
-from tempfile import SpooledTemporaryFile
+import io
 
+sys.path.append(".")
 
 api = FastAPI(docs_url="/")
 
@@ -15,16 +16,34 @@ async def upload(
 ):
   if file.content_type ==  "image/tiff":
     nfile = tiff_to_pdf(file)
+    print(nfile)
     return {nfile}
 
 def tiff_to_pdf(file:  UploadFile):
+  if os.path.isdir("files") == False:
+    os.mkdir("files")
+    return tiff_to_pdf(file)
+  else:
+    output = io.BytesIO()
+    image = Image.open(file.file._file)
+    output.seek(0, 0)
+    pdf_path = "files/" + file.filename.lower().replace('.tif', '.pdf' )
+  
+    images = []
+    for i, page in enumerate(ImageSequence.Iterator(image)):
+        page = page.convert("RGB")
+        images.append(page)
+    if len(images) == 1:
+        n_file = UploadFile(file.filename.lower().replace('.tif', '.pdf'), output)
+        images[0].save(pdf_path)
+    else:
+        n_file = UploadFile(file.filename.lower().replace('.tif', '.pdf'), output)
+        images[0].save(pdf_path, save_all=True,append_images=images[1:])
+    
+    os.remove(pdf_path)
 
-  output = io.BytesIO()
-  Image.open(file.file._file).convert("RGB").save(output, format="pdf")
-  output.seek(0, 0)
-  n_file = UploadFile('file.pdf', output)
-  return n_file
-
+    return n_file
+  
 
 if __name__ =='__main__':
   import uvicorn
